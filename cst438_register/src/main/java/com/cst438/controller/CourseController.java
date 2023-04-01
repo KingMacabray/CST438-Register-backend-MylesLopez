c0fa9dfa054bbff38cf5dc656217756cc466264a
Myles Lopez <mylopez@csumb.edu>
2023-04-01 04:33:31 -0700

    First iteration of course controller

--- cst438_register/src/main/java/com/cst438/controller/CourseController.java
+++ cst438_register/src/main/java/com/cst438/controller/CourseController.java
@@ -25,7 +25,15 @@ public class CourseController {
 	public void updateCourseGrades( @RequestBody CourseDTOG courseDTO, @PathVariable("course_id") int course_id) {
 		
 		//TODO  complete this method in homework 4
-		
+		// Create loop catch for getting course grade
+		for (CourseDTOG.GradeDTO courseGrade : courseDTO.grades) {
+			Enrollment enrolled = enrollmentRepository.findByEmailAndCourseId(courseGrade.student_email, course_id);
+			
+			//set the grade and save to the enrollment table
+			enrolled.setCourseGrade(courseGrade.grade);
+			enrollmentRepository.save(enrolled);
+		}
+
 	}
 
 }
