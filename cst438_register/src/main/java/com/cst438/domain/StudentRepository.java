3889fe9d816cfa4646007c86681db982a5eea508
Myles Lopez <mylopez@csumb.edu>
2023-04-01 19:34:42 -0700

    Updated StudentRepository.java and getting springboot to work

--- cst438_register/src/main/java/com/cst438/domain/StudentRepository.java
+++ cst438_register/src/main/java/com/cst438/domain/StudentRepository.java
@@ -1,11 +1,16 @@
 package com.cst438.domain;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface StudentRepository extends CrudRepository <Student, Integer> {
 	
 	// declare the following method to return a single Student object
 	// default JPA behavior that findBy methods return List<Student> except for findById.
-	public Student findByEmail(String email);
+	@Query("select s from Student s where s.email=:email")
+	Student findByEmail(@Param("email") String email);
 
 }
